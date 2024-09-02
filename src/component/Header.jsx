5f12a70da333f82872c16e9b277564f674015db6
vincent <vincentorium@gmail.com>
2024-09-02 21:39:37 +0800

    add nav bar

---
+++ src/component/Header.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const navItems = ['Welcome', 'Learning', 'Admission', 'Addresses'];
+
+function Header() {
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+
+      <CssBaseline />
+     <AppBar component="nav" className="appbarThing">
+        <Toolbar sx={{
+          borderTop: 15,
+          borderBottom: 15,
+          borderColor: 'rgb(130, 0, 36)',
+          backgroundColor: '#041138',
+          height: 90,
+        }}>
+          <Box
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 0, height: '100%' }}
+          >
+            <img
+              src='asset/yorkBadge.png'
+              alt=""
+              style={{ height: '100%' }}
+            />
+          </Box>
+
+
+          <Box
+            component="div"
+            sx={{
+              flexGrow: 4,
+              display: { xs: 'none', sm: 'block' },
+              marginTop: 4
+            }}
+          >   <Button variant="text"
+            sx={{
+              color: '#fff',
+              paddingRight: 0,
+              minWidth: '0',
+              fontSize: '1.1rem',
+              fontWeight: 900,
+              '&:hover': {
+                color: '#820632'
+              }
+            }}
+          >ENG</Button>
+            /
+            <Button variant="text"
+              sx={{
+                color: '#fff',
+                paddingLeft: 0,
+                minWidth: '0',
+                fontSize: '1.1rem',
+                fontWeight: 900,
+                '&:hover': {
+                  color: '#820632'
+                }
+              }}
+            >中</Button>
+            <img src='asset/languageIcon.png'
+              alt=""
+              style={{ width: '0.85rem' }}
+            />
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, backgroundColor: '#041138' }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{
+                color: '#fff', fontSize: 20, fontWeight: 600,
+                textTransform: 'capitalize',
+                marginRight: 1,
+                '&:hover': {
+                  color: '#820632'
+                },
+              }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+
+
+export default Header;
---
+++ src/page/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Fragment } from 'react';
+import Header from '../component/Header'
+import { Box } from '@mui/material';
+
+const Homepage = () => {
+
+
+  return (
+    <>
+    <Header></Header>
+      hello
+    </>
+  )
+};
+export default Homepage;
